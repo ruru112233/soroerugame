f50bb95c8c038b834a3b9275f21012225fd5bde0
ruru112233 <esxwaz1234@gmail.com>
2022-05-07 02:17:11 +0900

    左右のボタン処理追加

--- Assets/Script/Controller/ColorController.cs
+++ Assets/Script/Controller/ColorController.cs
@@ -17,17 +17,45 @@ namespace Controller
         //                                           { 0, 0, 0, 0, 0 },
         //                                           { 0, 0, 0, 0, 0 }};
 
-        private int[,] initColorNum = new int[,] { { 1, 2, 3, 4, 5 },
-                                                   { 3, 2, 3, 3, 5 },
-                                                   { 1, 5, 2, 4, 4 },
-                                                   { 1, 5, 1, 1, 1 },
-                                                   { 5, 4, 5, 2, 4 }};
+        //private int[,] initColorNum = new int[,] { { 1, 2, 3, 4, 5 },
+        //                                           { 3, 2, 3, 3, 5 },
+        //                                           { 1, 5, 2, 4, 4 },
+        //                                           { 1, 5, 1, 1, 1 },
+        //                                           { 5, 4, 5, 2, 4 }};
+
+        private List<List<int>> initColorNum = new List<List<int>>();
+
+        List<int> list1 = new List<int>() { 1, 2, 3, 4, 5 };
+        List<int> list2 = new List<int>() { 1, 2, 3, 4, 5 };
+        List<int> list3 = new List<int>() { 1, 2, 3, 4, 5 };
+        List<int> list4 = new List<int>() { 1, 2, 3, 4, 5 };
+        List<int> list5 = new List<int>() { 1, 2, 3, 4, 5 };
+
+
+
+        public List<List<int>> InitColorNum
+        {
+            get => initColorNum;
+            set => initColorNum = value;
+        }
+
+        public List<int> List1
+        {
+            set => initColorNum[0] = value;
+        }
 
         private int elementCount = 0;
         Color color;
 
         void Start()
         {
+            
+            initColorNum.Add(list1);
+            initColorNum.Add(list2);
+            initColorNum.Add(list3);
+            initColorNum.Add(list4);
+            initColorNum.Add(list5);
+
             ChengePanelColor();
         }
 
@@ -37,53 +65,25 @@ namespace Controller
         }
 
         void ChengePanelColor()
-        {
-            for (int y = 0; y < initColorNum.GetLength(0); y++)
+        {   
+
+            for (int y = 0; y < initColorNum.Count; y++)
             {
-                for (int x = 0; x < initColorNum.GetLength(1); x++)
+                for (int x = 0; x < initColorNum[y].Count; x++)
                 {
-                    
+
                     Image image = panelManager.panelImageList[elementCount].GetComponent<Image>();
 
-                    int colorNum = initColorNum[y, x];
+                    int colorNum = initColorNum[y][x];
 
-                    image.color = selectColor(colorNum);
+                    image.color = Util.selectColor(colorNum);
 
                     elementCount++;
                 }
             }
+
         }
 
-        Color selectColor(int colorNum)
-        {
-            
-            if (colorNum == 1)
-            {
-                color = Color.red;
-            }
-            else if (colorNum == 2)
-            {
-                color = Color.yellow;
-            }
-            else if (colorNum == 3)
-            {
-                color = Color.green;
-            }
-            else if (colorNum == 4)
-            {
-                color = Color.blue;
-            }
-            else if (colorNum == 5)
-            {
-                color = Color.cyan;
-            }
-            else
-            {
-                color = Color.white;
-            }
-
-            return color;
-        }
     }
 
 }
